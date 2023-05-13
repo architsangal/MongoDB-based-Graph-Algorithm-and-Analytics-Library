16c95b5a33796fe13b502be57941db2c1b48a2dc
architsangal <architsangal2000@gmail.com>
2023-05-13 13:58:14 +0530

    Rearranging

--- Aditya/create_database/createDB.mongodb.js
+++
@@ -1,3 +0,0 @@
-db = connect( 'mongodb://localhost:27017/' + dbName );
-db.dropDatabase();
-db = db.getSiblingDB(dbName);
--- Aditya/transformations/topological_minor/nameOfCollection.js
+++
@@ -1,3 +0,0 @@
-inputCollection = "petersen"
-intermediateCollection = "intermediatepetersen"
-outputCollection = "c5"
